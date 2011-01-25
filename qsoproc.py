5a87a14acc6c7bd25686fb43bc6b897844057d40
Steve Conklin <sconklin@canonical.com>
2011-01-25 11:52:02 -0600

    Added clock face, colors, fixed geometry

--- qsoproc.py
+++ qsoproc.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python
 #
 #import re
+from math import sin, cos, radians
+
+
 from Hamlib import *
 from adif import *
-from pysvg.structure import *
+
+from pysvg.filter import *
+from pysvg.gradient import *
+from pysvg.linking import *
+from pysvg.script import *
 from pysvg.shape import *
-import math
+from pysvg.structure import *
+from pysvg.style import *
+from pysvg.text import *
+from pysvg.builders import *
+
 
 #
 # based on a 24 hour clock
@@ -29,17 +40,67 @@ def bearingDistance(myLoc, oLoc):
     return (distance, azimuth)
 
 def polar2Rect(distance, angle):
-    x = distance * math.cos(angle)
-    y = distance * math.sin(angle)
+    x = distance * cos(radians(angle))
+    y = distance * sin(radians(angle))
     return (x, y)
+
+def freq2Color(freqstr):
+    freq = float(freqstr)
+    if (freq >= 1.8) and (freq <= 2.0):
+        # 160m
+        return 'lawngreen';
+    elif (freq >= 3.5) and (freq <= 4.0):
+        # 80m
+        return 'darkviolet';
+    elif (freq >= 5.3) and (freq <= 5.5):
+        # 60m
+        return 'midnightblue';
+    elif (freq >= 7.0) and (freq <= 7.3):
+        # 40m
+        return 'cornflowerblue';
+    elif (freq >= 10.1) and (freq <= 10.150):
+        # 30m
+        return 'seagreen';
+    elif (freq >= 14.0) and (freq <= 14.350):
+        # 20m
+        return 'goldenrod';
+    elif (freq >= 18.068) and (freq <= 18.168):
+        # 17m
+        return 'khaki';
+    elif (freq >= 21.0) and (freq <= 21.450):
+        # 15m
+        return 'tan';
+    elif (freq >= 24.890) and (freq <= 24.990):
+        # 12m
+        return 'brown';
+    elif (freq >= 28.0) and (freq <= 29.7):
+        # 10m
+        return 'mediumorchid';
+    elif (freq >= 50.0) and (freq <= 54.0):
+        # 6m
+        return 'red';
+    else:
+        # Unknown 
+        return 'dimgrey';
 
 if __name__ == '__main__':
 
-    scale = 500
-    xoff = 20
-    yoff = 20
+    scale = 50
+    dotsize = 5
+    xoff = 200
+    yoff = 200
+    timelength = 180
 
     sz = svg("My test")
+    oh = ShapeBuilder()
+
+    idcount = 0
+
+    # draw 24 hour markers
+    for hour in range (0, 24):
+        ang = hour * 15
+        tx, ty = polar2Rect(timelength, ang)
+        sz.addElement(oh.createLine(xoff, yoff, xoff + tx, yoff + ty, strokewidth=2, stroke="black"))
 
     recs = adifParse(sys.argv[1])
     for rec in recs:
@@ -48,16 +109,25 @@ if __name__ == '__main__':
         if not rec.has_key('gridsquare'):
             print "skipping " + call
             continue
+        if not rec.has_key('freq'):
+            print "skipping " + call
+            continue
 
         angle = angleFromTime(rec["time_on"])
         distance, azimuth = bearingDistance(rec["my_gridsquare"], rec["gridsquare"])
         distance = distance / scale
-        print distance, azimuth
+        #print distance, azimuth
         bx, by = polar2Rect(distance, azimuth)
-        blob = circle(bx + xoff, by + yoff, 0.25)
-        sz.addElement(blob)
-        #print call, angle, distance
-        #print rec["freq_rx"]
+        fillcolor = freq2Color(rec["freq"])
+
+        eid = str(idcount)
+
+        dot = circle(bx + xoff, by + yoff, dotsize)
+        dot.set_fill(fillcolor)
+        dot.set_stroke('black')
+
+        sz.addElement(dot)
+
 
     sz.save("./testout.svg")
 
